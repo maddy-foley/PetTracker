16559e99ca676754ce4cd0a5961ad6fda045dc81
Maddy Foley <maddyfoley5@gmail.com>
2025-08-08 16:03:40 -0400

    initial commit, setting up header

---
+++ PetTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetTracker
+//
+//  Created by Maddy Foley on 8/8/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var items: [Item]
+
+    var body: some View {
+        HeaderView()
+    }
+
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
---
+++ PetTracker/HeaderView.swift
@@ -0,0 +1,33 @@
+//
+//  HeaderView.swift
+//  PetTracker
+//
+//  Created by Maddy Foley on 8/8/25.
+//
+
+import SwiftUI
+
+
+struct HeaderView: View {
+    @Environment(\.modelContext) private var modelContext
+
+
+    var body: some View {
+        ZStack{
+            VStack(alignment: .leading){
+                HStack(){
+                    Text("Left")
+                        .padding(.leading)
+                    Spacer()
+                    Text("Right")
+                        .padding(.trailing)
+                    }
+                    
+                }
+                
+            }
+            Spacer()
+        }
+    }
+
+
